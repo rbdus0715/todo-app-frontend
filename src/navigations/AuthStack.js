80afa034942080e9e37a485ce281f8d5836f2037
rbdus0715 <rbdus0715@gmail.com>
2025-10-09 12:08:44 +0900

    Feat: 이전 버튼 로고 수정 및 뒤로가기 로직 추가
    - useNavigation 훅 사용

--- src/navigations/AuthStack.js
+++ src/navigations/AuthStack.js
@@ -2,6 +2,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignInScreen from '../screens/SignInScreen';
 import ListScreen from '../screens/ListScreen';
 import { PRIMARY, WHITE } from '../color';
+import { Pressable, Text } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +15,20 @@ const AuthStack = () => {
       screenOptions={{
         contentStyle: { backgroundColor: WHITE },
         headerTitleAlign: 'center',
+        headerLeft: ({ canGoBack, tintColor }) => {
+          const navigation = useNavigation();
+
+          if (!canGoBack) return null;
+          return (
+            <Pressable onPress={navigation.goBack}>
+              <MaterialCommunityIcons
+                name="chevron-left"
+                size={30}
+                color={tintColor}
+              />
+            </Pressable>
+          );
+        },
       }}
     >
       <Stack.Screen
@@ -24,6 +41,13 @@ const AuthStack = () => {
             fontWeight: '700',
             // color: 'lightcoral',
           },
+          //   headerTitle: ({ children, tintColor }) => {
+          //     return (
+          //       <Pressable onPress={() => console.log('test')}>
+          //         <Text style={{ color: tintColor }}>{children}</Text>
+          //       </Pressable>
+          //     );
+          //   },
         }}
       />
       <Stack.Screen
