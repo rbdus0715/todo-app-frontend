3bd5744ad3f3d8800e9bd69d334641c94f7a1181
rbdus0715 <rbdus0715@gmail.com>
2025-10-09 12:14:23 +0900

    Refactor: HeaderLeftButton 컴포넌트 분리
    + hitSlop 10 옵션 - 근처 클릭해도 작동하도록

--- src/navigations/AuthStack.js
+++ src/navigations/AuthStack.js
@@ -2,9 +2,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignInScreen from '../screens/SignInScreen';
 import ListScreen from '../screens/ListScreen';
 import { PRIMARY, WHITE } from '../color';
-import { Pressable, Text } from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import HeaderLeftButton from '../components/HeaderLeftButton';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,20 +13,7 @@ const AuthStack = () => {
       screenOptions={{
         contentStyle: { backgroundColor: WHITE },
         headerTitleAlign: 'center',
-        headerLeft: ({ canGoBack, tintColor }) => {
-          const navigation = useNavigation();
-
-          if (!canGoBack) return null;
-          return (
-            <Pressable onPress={navigation.goBack}>
-              <MaterialCommunityIcons
-                name="chevron-left"
-                size={30}
-                color={tintColor}
-              />
-            </Pressable>
-          );
-        },
+        headerLeft: HeaderLeftButton,
       }}
     >
       <Stack.Screen
